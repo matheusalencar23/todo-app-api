d1b8a833b3cadfedfe67dddd100e77c9efd70eb4
Matheus Alencar <matheusalencar6942@gmail.com>
2023-03-29 15:29:09 -0300

    creating todo module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './db/typeOrmConfig.service';
+import { TodoModule } from './app/todo/todo.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { TypeOrmConfigService } from './db/typeOrmConfig.service';
       inject: [ConfigService],
       useClass: TypeOrmConfigService,
     }),
+    TodoModule,
   ],
   controllers: [],
   providers: [],
